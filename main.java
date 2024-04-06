790d17b188790be5312910c24405dbd5a7ddd34e
TudorOsan03 <166154384+TudorOsan03@users.noreply.github.com>
2024-04-06 20:03:37 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+
+public class main {
+    public static void Main(String[] args ) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("test");
+        window.setLocationRelativeTo(null);
+
+    }
+}
