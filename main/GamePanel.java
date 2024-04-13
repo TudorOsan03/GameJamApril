c3bb19fd5229827dc1469cfa5db5ba1d6e501e55
TudorOsan03 <166154384+TudorOsan03@users.noreply.github.com>
2024-04-13 21:41:56 +0300

    Add files via upload

--- main/GamePanel.java
+++ main/GamePanel.java
@@ -5,12 +5,14 @@ import java.awt.Dimension;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     final int ogtilesize = 16;
     final int scale = 3;
     final int tilesize = ogtilesize * scale;
     final int screenwidth = tilesize * 16;
     final int screenheight = tilesize * 12;
+
+    Thread GameThread;
 
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenwidth, screenheight));
@@ -18,4 +20,17 @@ public class GamePanel extends JPanel {
         this.setDoubleBuffered(true);
     }
 
+
+    public void GameStartThread(){
+        GameThread= new Thread(this);
+        GameThread.start();
+    }
+
+    @Override
+    public void run(){
+        while (GameThread!=null) {
+            
+            
+        }
+    }
 }
